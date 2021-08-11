39403a3197fa5643aaf618bcde326f0810842187
Maxim Kaziev <kazievm93@mail.ru>
2021-08-11 21:30:09 +0300

    Готовое первое задание

--- task_1/solution.js
+++ task_1/solution.js
@@ -13,7 +13,7 @@ function addInCartMessage(title, price) {
     // добавьте в конец переменной message пробел
     // добавьте в конец переменной message текст 'теперь в корзине!'
     // выведите переменную message в консоль
-    let message = String(productName + 'за' + productPrice + 'теперь в корзине!');
+    let message = String(productName + ' за ' + productPrice + ' теперь в корзине!');
     console.log(message);
     // Конец решения задания №1.1.
 
